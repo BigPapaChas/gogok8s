e042b03d424a7920a03b1ced98f284cc0c572333
Chase Andries <candries@squareup.com>
2021-12-27 23:51:54 -0500

    Bump CLI version to v0.0.6

--- internal/commands/root.go
+++ internal/commands/root.go
@@ -19,7 +19,7 @@ var (
 var rootCmd = &cobra.Command{
 	Use:     "gogok8s",
 	Short:   "gogok8s helps manage your k8s cluster kubeconfig(s)",
-	Version: "v0.0.5",
+	Version: "v0.0.6",
 }
 
 func init() {
